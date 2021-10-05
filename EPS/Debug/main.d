4a5c40a53c0528c1b5dbe466217ada131a88d3f0
Craig Opie <craigopie@gmail.com>
2021-10-04 16:43:04 -1000

    updated the project files for Code Composer Studio

--- EPS/Debug/main.d
+++ EPS/Debug/main.d
@@ -1,10 +1,13 @@
 # FIXED
 
 main.o: ../main.c \
+ /Applications/ti/ccs1040/ccs/ccs_base/msp430/include_gcc/msp430.h \
  /Applications/ti/ccs1040/ccs/ccs_base/msp430/include_gcc/msp430fr6989.h \
  /Applications/ti/ccs1040/ccs/ccs_base/msp430/include_gcc/iomacros.h \
  /Applications/ti/ccs1040/ccs/ccs_base/msp430/include_gcc/in430.h \
  ../uart.h
+
+/Applications/ti/ccs1040/ccs/ccs_base/msp430/include_gcc/msp430.h:
 
 /Applications/ti/ccs1040/ccs/ccs_base/msp430/include_gcc/msp430fr6989.h:
 
